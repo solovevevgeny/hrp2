f6b289885f750e07c4cfa99bce81fc09b76aac83
Solovev Evgenym <evgeny.solovev@gmail.com>
2021-07-30 13:28:24 +0300

    create models (product, offer, warehouse, offer_status)

---
+++ database/migrations/2021_07_30_102220_create_warehouses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWarehousesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('warehouses', function (Blueprint $table) {
+            $table->id();
+            $table->string("xml_id");
+            $table->string("title");
+            $table->string("address");
+            $table->string("phone");
+            $table->string("time");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('warehouses');
+    }
+}
