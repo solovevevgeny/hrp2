bbf02ec76de83fbf0d75b41c50366ad99d9a3786
Solovev Evgenym <evgeny.solovev@gmail.com>
2021-08-02 14:46:12 +0300

    relationships on Offers

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Offer;
 
 class ProductController extends Controller
 {
     public function index() 
     {
-        $products = Product::all();
+        $products = Product::with('offers')->get();
         return response ($products, 200);
     }
 
+    public function offers ($productId) {
+        $offers = Offer::find();
+    }
 
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public function offers() {
+        return $this->hasMany(\App\Models\Offer::class, 'parent_id', 'id');
+    }
+
 }
